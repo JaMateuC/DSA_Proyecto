08a9442aedd6ec808410d43da5b408c127c96613
oscar <oscarmampel@gmail.com>
2017-10-06 11:47:46 +0200

    Changes to be committed: 	modified:   .gitignore 	modified:   src/main/java/eetac/dsa/Celda.java

--- src/main/java/eetac/dsa/Celda.java
+++ src/main/java/eetac/dsa/Celda.java
@@ -1,4 +1,28 @@
 package eetac.dsa;
 
-public class Celda {
+/**
+ * La clase celda es una plantilla para cada conjunto de propiedades que tiene cada una de las posiciones del mapa
+ */
+public abstract class Celda
+{
+    String tipo;
+    /**
+     * probabilidad con la que aparece un objeto en esta celda
+     */
+    float probObjeto;
+
+    /**
+     * probabilidad con la que aparece un monstruo
+     */
+    float probMonstruo;
+
+    Boolean anadable;
+
+    /**
+     * si existe un personaje encima esta propiedad es diferente de null.
+     * solo un personaje por celda.
+     */
+    Personaje personajeEncima;
+
+
 }
