b2450c80531c9a9d26a464a56707e93be4de1e7f
Albert <albertalvarezmiron@gmail.com>
2017-10-06 12:04:43 +0200

    Changes to be committed: 	modified:   src/main/java/eetac/dsa/Personaje.java

--- src/main/java/eetac/dsa/Personaje.java
+++ src/main/java/eetac/dsa/Personaje.java
@@ -1,4 +1,14 @@
 package eetac.dsa;
 
-public class Personaje {
+/**
+ * La clase Personaje otorga los atributos comunes a Usuario y Enenmigo
+ */
+public abstract class Personaje
+{
+    private String nombre;
+    private String escenario;
+    private int x, y;
+    private Inventario inventario;
+    private Lista_Monstruos lista_montruos;
+    private boolean genero;
 }
