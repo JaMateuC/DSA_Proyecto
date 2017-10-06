87b9fe9edc1def8a763b86c1709d377f4dec8a47
Albert <albertalvarezmiron@gmail.com>
2017-10-06 12:22:31 +0200

    Changes to be committed: 	modified:   src/main/java/eetac/dsa/Personaje.java 	modified:   src/main/java/eetac/dsa/Usuario.java

--- src/main/java/eetac/dsa/Personaje.java
+++ src/main/java/eetac/dsa/Personaje.java
@@ -8,7 +8,18 @@ public abstract class Personaje
     private String nombre;
     private String escenario;
     private int x, y;
-    private Inventario inventario;
     private Lista_Monstruos lista_montruos;
+    private Inventario inventario;
     private boolean genero;
+
+    public Personaje(String nombre, String escenario, int x, int y, Lista_Monstruos lista_montruos, Inventario inventario, boolean genero)
+    {
+        this.nombre = nombre;
+        this.escenario = escenario;
+        this.x = x;
+        this.y = y;
+        this.lista_montruos = lista_montruos;
+        this.inventario = inventario;
+        this.genero = genero;
+    }
 }
--- src/main/java/eetac/dsa/Usuario.java
+++ src/main/java/eetac/dsa/Usuario.java
@@ -3,8 +3,15 @@ package eetac.dsa;
 /**
  * La Clase Usuario permite instanciar un personaje para el jugador
  */
-public class Usuario
+public class Usuario extends Personaje
 {
     private String email;
     private String password;
+
+    public Usuario(String nombre, String escenario, int x, int y, Lista_Monstruos lista_montruos, Inventario inventario, boolean genero, String email, String password)
+    {
+        super(nombre, escenario, x, y, lista_montruos, inventario, genero);
+        this.email = email;
+        this.password = password;
+    }
 }
