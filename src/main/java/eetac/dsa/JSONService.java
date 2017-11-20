02e5af9a737c8aa1c255268b260944380b0bd87b
Albert <albertalvarezmiron@gmail.com>
2017-11-20 12:56:20 +0100

    rest

--- src/main/java/eetac/dsa/JSONService.java
+++ src/main/java/eetac/dsa/JSONService.java
@@ -1,40 +1,44 @@
 package eetac.dsa;
 
+import eetac.dsa.Servidor.MundoControlador;
+import eetac.dsa.Servidor.ResultadoServidor;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/json")
 
-public class JSONService {
-    @GET
-    @Path("/got/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public int getTrack(@PathParam("id") int id) {
-        return 0;
-    }
-
-    @GET
-    @Path("/get")
-    @Produces(MediaType.APPLICATION_JSON)
-    public int getTrackInJSON() {
-        return 1;
+public class JSONService
+{
+    @POST
+    @Path("/{username}/action")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public ResultadoServidor action(@PathParam("username") String userName, int x, int y)
+    {
+        return MundoControlador.getInstance().getSesion(userName).hacerAccion(x, y);
     }
 
     @POST
-    @Path("/new")
+    @Path("/{username}/position")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response newTrack(int track) {
-        // Atencion: siempre añade en la misma posicion por el scope de tracks
-        return Response.status(201).entity("Track added in position").build();
+    public ResultadoServidor position(@PathParam("username") String userName, int x, int y)
+    {
+        return MundoControlador.getInstance().getSesion(userName).mover(x, y);
     }
 
     @POST
-    @Path("/post")
+    @Path("/{username}/usarObjeto/monstruo")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response createTrackInJSON(int track) {
+    public ResultadoServidor useObject(@PathParam("username") String userName, int indexObjeto, int indexMonstruo)
+    {
+        return MundoControlador.getInstance().getSesion(userName).usarObjetoMonstruo(indexObjeto, indexMonstruo);
+    }
 
-        String result = "Track saved : " + track;
-        return Response.status(201).entity(result).build();
+    @POST
+    @Path("/{username}/usarObjeto/protagonista")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public ResultadoServidor useObject(@PathParam("username") String userName, int index)
+    {
+        return MundoControlador.getInstance().getSesion(userName).usarObjetoProtagonista(index);
     }
 }
