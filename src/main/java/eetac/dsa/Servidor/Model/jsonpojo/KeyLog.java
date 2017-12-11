cda98a1c50aee43c2db40057a4d739376b8a7495
Albert Alvarez <albertalvarezmiron@gmail.com>
2017-12-11 20:30:36 +0100

    key ok

---
+++ src/main/java/eetac/dsa/Servidor/Model/jsonpojo/KeyLog.java
@@ -0,0 +1,12 @@
+package eetac.dsa.Servidor.Model.jsonpojo;
+
+public class KeyLog
+{
+    private int key;
+
+    public KeyLog() { }
+
+    public int getKey() { return key; }
+
+    public void setKey(int key) { this.key = key; }
+}
--- src/main/java/eetac/dsa/Servidor/Service/AUTHservice.java
+++ src/main/java/eetac/dsa/Servidor/Service/AUTHservice.java
@@ -1,9 +1,11 @@
 package eetac.dsa.Servidor.Service;
 
+import eetac.dsa.Servidor.Model.jsonpojo.KeyLog;
 import eetac.dsa.Servidor.Model.jsonpojo.UserLog;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.security.Key;
 import java.util.Random;
 
 @Path("/auth")
@@ -11,11 +13,19 @@ public class AUTHservice
 {
     @POST
     @Path("/login")
-    @Produces(MediaType.TEXT_PLAIN)
-    public int login(UserLog user)
+    @Produces(MediaType.APPLICATION_JSON)
+    public KeyLog login(UserLog user)
     {
-        //  A partir de UserLog se consulta la BBDD y devuelve la key de acceso
-        if(user.getPassword().equals("123")) { return new Random().nextInt(2048); }
-        return -1;
+        //  A partir de UserLog (nombre, password) se consulta la memoria y devuelve la key de acceso
+
+        KeyLog key = new KeyLog();
+        if(user.getPassword().equals("123"))    //En el equals va la respuesta de la memoria (password)
+        {
+            key.setKey(new Random().nextInt(2048));
+            return key;
+        }
+
+        key.setKey(-1);
+        return key;
     }
 }
